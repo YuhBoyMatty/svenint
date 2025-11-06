89b466f86293bae1ae592ca406736db8c7bc2fe4
Sw1ft <mgh.stirges@mail.ru>
2025-11-06 22:33:25 +0500

    Clean up

--- resources/sven_internal/scripts/test.lua
+++
@@ -1,8 +0,0 @@
-function OnTouchTrigger(name)
-	printl( "Touched trigger: \"" .. name .. "\"" );
-end
-
-if ( GetMapName():lower() == "hl_c01_a1" ) then
-	CreateTrigger("test_trigger", Vector(104.531, 169.531, -209.969), Vector(0, 0, 0), Vector(98.281, 210.438, 78.991));
-	printl( "Created trigger \"test_trigger\" for current map \"hl_c01_a1\"" );
-end
