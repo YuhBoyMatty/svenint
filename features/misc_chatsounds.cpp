b3c39b3c6881abcd4d1b7d45babac9b164811fdf
Sw1ft <mgh.stirges@mail.ru>
2025-12-23 20:21:11 +0500

    Add Chat Sounds

---
+++ features/misc_chatsounds.cpp
@@ -0,0 +1,5 @@
+// SvenInt (c) Sw1ft
+// misc_chatsounds.cpp
+
+#include "stdafx.h"
+#include "misc_chatsounds.h"
---
+++ features/misc_chatsounds.h
@@ -0,0 +1,13 @@
+// SvenInt (c) Sw1ft
+// misc_chatsounds.h
+
+#ifndef SINT_FEATURE_CHAT_SOUNDS_H
+#define SINT_FEATURE_CHAT_SOUNDS_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+
+
+#endif // SINT_FEATURE_CHAT_SOUNDS_H
