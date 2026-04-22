10f97fbe5d918cf95b7b995ea23e23d946d7cf95
Sw1ft <mgh.stirges@mail.ru>
2026-04-22 20:31:57 +0500

    Add private feature

---
+++ features/misc_chat_logger.cpp
@@ -0,0 +1,5 @@
+// SvenInt (c) Sw1ft
+// misc_chat_logger.cpp
+
+#include "stdafx.h"
+#include "misc_chat_logger.h"
---
+++ features/misc_chat_logger.h
@@ -0,0 +1,13 @@
+// SvenInt (c) Sw1ft
+// misc_chat_logger.h
+
+#ifndef SINT_FEATURE_CHAT_LOGGER_H
+#define SINT_FEATURE_CHAT_LOGGER_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+
+
+#endif // SINT_FEATURE_CHAT_LOGGER_H
